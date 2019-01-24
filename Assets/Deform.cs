a70d2dc24032637db645a2b15dfd4a5a03e3c7cf
ElementMo <869077498@qq.com>
2019-01-24 23:23:54 +0800

    done

---
+++ Assets/Deform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public enum Axis
+{
+    x, y, z
+};
+
+public class Deform : MonoBehaviour { 
+
+    Mesh mesh;
+    Vector3[] initVertices;
+    Vector3[] vertices;
+    Vector3[] noiseVertices;
+
+    [SerializeField] float amount = 30.0f;
+    [SerializeField] float frequency = 1.0f;
+    [SerializeField] float smooth = 1.0f;
+    [SerializeField] Axis axis  = Axis.z;
+
+    private void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        initVertices = mesh.vertices;
+        noiseVertices = mesh.vertices;
+    }
+    void Update()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            if (axis == Axis.z)
+                noiseVertices[i].z = initVertices[i].z + (amount * Mathf.PerlinNoise(Time.time * frequency + i * smooth, 0));
+            if (axis == Axis.x)
+                noiseVertices[i].x = initVertices[i].x + (amount * Mathf.PerlinNoise(Time.time * frequency + i * smooth, 0));
+            if (axis == Axis.y)
+                noiseVertices[i].y = initVertices[i].y + (amount * Mathf.PerlinNoise(Time.time * frequency + i * smooth, 0));
+        }
+
+        mesh.vertices = noiseVertices;
+    }
+}
